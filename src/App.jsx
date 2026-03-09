18b2d1d75e22de69dd5f9f073ba5808668e9e06d
Patrick Park <patrickpark208@gmail.com>
2026-03-09 14:59:45 -0700

    Adding vercel usage tracking and ads.txt

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import AppShell from './app/AppShell'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './App.css'
 
 function App() {
-  return <AppShell />
+  return (
+    <>
+      <AppShell />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  )
 }
 
 export default App
